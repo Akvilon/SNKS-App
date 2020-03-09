cf0b4a8f2a39b6913741b86f130dd2ef7ad36637
Eugen Pavlov <Eugen.Pavlov@gmail.com>
2020-03-09 21:00:19 +0200

    added basic structure (v2)

---
+++ src/components/Header/Header.component.container.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+
+const ConnectedHeaderContainer = '';
+
+export {ConnectedHeaderContainer as HeaderContainer};
---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+type HeaderProps = {};
+
+const Header = (props: HeaderProps) => {
+  return (
+    <>
+      <p>Header</p>
+    </>
+  );
+};
+
+export {Header};
